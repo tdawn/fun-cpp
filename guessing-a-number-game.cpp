15c700caace6827ec00d9b9941b38033fe3cc3d9
Dwi Ary <tdawn.id@gmail.com>
2024-09-18 15:37:44 +0700

    Update guessing-a-number-game.cpp

--- guessing-a-number-game.cpp
+++ guessing-a-number-game.cpp
@@ -1,7 +1,6 @@
 //Objective	: to create a "guess the number" game
-//Rules		:
-//	- System will generate random number
-//	- User should make a guess, if the the guess is smaller or higher, the code will give feedback
+//Rules		: - System will generate random number
+//		  - User should make a guess, if the the guess is smaller or higher, the code will give feedback
 //Input		: a guess
 //Output	: feedback and number of attempts
 
@@ -15,13 +14,16 @@ int main(){
 	srand((int) time(0));
 	
 	int random_number, n, lb = 0, ub = 100, attempt = 1;
+	
 	random_number = rand() % 101;
 	
 	cout << "Welcome to GUESS THE NUMBER game!\n"
 		<< "Choose any number within 0-100 to guess the number.\n\n";
+	
 	do{
 		cout << "Enter your guess: ";
 		cin >> n;
+		
 		if (n < lb || n > ub){
 			cout << "Choose number within " << lb << " to " << ub << ".\n\n";
 			continue;
