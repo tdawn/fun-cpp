acb9b74248f7c4de5d40010e616d546c7f7d52ca
Dwi Ary <tdawn.id@gmail.com>
2024-09-18 15:34:40 +0700

    Add files via upload

---
+++ calculating-factorial-of-a-number.cpp
@@ -0,0 +1,19 @@
+//Objective	: to calculate factorial of a number
+//Input		: a number
+//Output		: factorial result of the input number
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, fact = 1;
+	
+	cout << "Enter a number\t: ";
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++){
+		fact *= i;
+	}
+	
+	cout << "fact(" << n << ")\t\t: " << fact;
+}
